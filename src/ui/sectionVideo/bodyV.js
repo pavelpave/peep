787d8d2c721a2c6a6bfbd800579944bb86972dca
pavelpave <arkhip-sergeev@mail.ru>
2019-03-03 21:39:38 +0300

    --all

--- src/ui/sectionVideo/bodyV.js
+++ src/ui/sectionVideo/bodyV.js
@@ -10,25 +10,29 @@ export default class BodyVideo extends Component{
 		this.ObnState = this.ObnState.bind(this)
 		this.state = {
 			ClassNameStrel : {
-				_1: [true, 0 , {recomend:true}],
-				_2: [false, 0, {translation:false}],
-				_3: [false, 0, {serch_kanal:false}],
-				_4: [false, 0, {podpis:false}],
-				_5: [false, 0, {create_pl:false}],
-				_6: [false, 0, {video_frend:false}],
-				_7: [false, 0, {pozje:false}],
-				_8: [false, 0, {history:false}],
-				_9: [false, 0, {izbran:false}],
-				_10: [false, 0, {my_post: false}]
+				_1: [true, podgruz()[0] , {recomend:true} , 1],
+				_2: [false, podgruz()[0], {translation:false} , 1],
+				_3: [false, 0, {serch_kanal:false} , 2],
+				_4: [false, 0, {podpis:false} , 2],
+				_5: [false, 0, {create_pl:false} , 3],
+				_6: [false, 0, {video_frend:false} , 3],
+				_7: [false, 0, {pozje:false} , 4],
+				_8: [false, 0, {history:false} , 4],
+				_9: [false, 0, {izbran:false} , 5],
+				_10: [false, 0, {my_post: false} , 5]
 			}
 		}
 		this.usstr = this.usstr.bind(this)
 	}
-	grid__video__system(){
-		return 2
+	grid__video__system(blockOut){
+		// if (blockOut) {
+		// 	return podgruz()
+		// }else{
+		// 	return
+		// }
 	}
 	usstr(e){
-		console.log(e);
+		// console.log(e);
 		
 		let index     = e.target.id,
 			keyTrue   = 0,
@@ -62,7 +66,7 @@ export default class BodyVideo extends Component{
 				 }
 			 }
 		}
-		console.log(e.target.id)
+		// console.log(e.target.id)
 		// this.setState(function (prevState, props) {
 		// 		return { 
 		// 			ClassNameStrel: this.state.ClassNameStrel
@@ -76,32 +80,37 @@ export default class BodyVideo extends Component{
 		this.setState(prevState = newState)
 	}
 	funSwapMath(s){
-		// console.log(s);
-		// for (const key in this.state.ClassNameStrel) {
-		// 	if (s == this.state.ClassNameStrel[key][0]) {
-		// 		console.log(this.state.ClassNameStrel[key][0]);
-		// 		return podgruz()
-		// 	}else{
-		// 		return podgruz()
-		// 	}
-		// }
+		console.log(s);
+		for (const key in this.state.ClassNameStrel) {
+			if (s[0] == this.state.ClassNameStrel[key][0]) {
+				return
+				
+			} else if (this.state.ClassNameStrel[key][3] == s[3]){
+				return 
+			}else{
+				return podgruz()[0]
+			}
+			
+		}
 	}
 	render(){
 		return(
 			<section className="sec__body">
 				<div className="sec__body__spis_one">
 				
-					<div className='sec__body__spis_one___rec' style={{ top: this.funSwapMath(this.state.ClassNameStrel._1[0]) }} >
+					<div className='sec__body__spis_one___rec' style={{ top: this.funSwapMath('_1') }} >
 						<div className="sec__body__spis_one___rec_icon_1"></div>
 						<a href="#">Рекомендации</a>
 						<div id="_1" className={this.state.ClassNameStrel._1[0] ? "sec__body__spis_one___rec_us active__icon" : "sec__body__spis_one___rec_us "}
 							onClick={this.usstr}></div>
 
 						<div className={this.state.ClassNameStrel._1[2].recomend ? "video__block__open" : "video__block__close"} style={{ height: this.state.ClassNameStrel._1[1] }}>
-							{this.grid__video__system()}
+							{this.grid__video__system(this.state.ClassNameStrel._1 )}
 						</div>
 					</div>
-					<div className="sec__body__spis_one___tra">
+
+
+					<div className="sec__body__spis_one___tra" style={{ top: this.funSwapMath(this.state.ClassNameStrel._2) }}>
 						<div className="sec__body__spis_one___rec_icon_2"></div>
 						<a href="#">Трансляции</a>
 						<div id="_2"
@@ -109,12 +118,12 @@ export default class BodyVideo extends Component{
 							onClick={this.usstr}
 						></div>
 
-						<div className={this.state.ClassNameStrel._2[2].translation ? "video__block__open" : "video__block__close"} style={{ height: this.state.ClassNameStrel._2[1] }}>
-							{this.grid__video__system()}
+						<div className={this.state.ClassNameStrel._2[2].translation ? "video__block__open_right" : "video__block__close"} style={{ height: this.state.ClassNameStrel._2[1] }}>
+							{this.grid__video__system(this.state.ClassNameStrel._2)}
 						</div>
 					</div>
 
-					<div className="sec__body__spis_one___sech">
+					<div className="sec__body__spis_one___sech" style={{ top: this.funSwapMath(this.state.ClassNameStrel._3) }}>
 						<div className="sec__body__spis_one___rec_icon_3"></div>
 						<a href="#">Найти канал</a>
 						<div id="_3" className={this.state.ClassNameStrel._3[0] ? "sec__body__spis_one___rec_us nejdan active__icon active_str_n" : "sec__body__spis_one___rec_us pasiv_us_strel"}
@@ -122,77 +131,77 @@ export default class BodyVideo extends Component{
 						</div>
 
 						<div className={this.state.ClassNameStrel._3[2].serch_kanal ? "video__block__open" : "video__block__close"} style={{ height: this.state.ClassNameStrel._3[1] }}>
-							{this.grid__video__system()}
+							{this.grid__video__system(this.state.ClassNameStrel._3)}
 						</div>
 					</div>
 
-					<div className="sec__body__spis_one___podpis" >
+					<div className="sec__body__spis_one___podpis" style={{ top: this.funSwapMath(this.state.ClassNameStrel._4) }}>
 						<div className="sec__body__spis_one___rec_icon_4"></div>
 						<a href="#">Подписки</a>
 						<div id="_4" className={this.state.ClassNameStrel._4[0] ? "sec__body__spis_one___rec_us_l  active_icon_two" : "sec__body__spis_one___rec_us_l"}
 							onClick={this.usstr}></div>
 						<div className={this.state.ClassNameStrel._4[2].podpis ? "video__block__open" : "video__block__close"} style={{ height: this.state.ClassNameStrel._4[1] }}>
-							{this.grid__video__system()}
+							{this.grid__video__system(this.state.ClassNameStrel._4)}
 						</div>
 					</div>
 
-					<div className="sec__body__spis_one___sech" >
+					<div className="sec__body__spis_one___sech" style={{ top: this.funSwapMath(this.state.ClassNameStrel._5) }}>
 						<div className="sec__body__spis_one___rec_icon_5"></div>
 						<a href="#">Создать плейлист</a>
 						<div id="_5" className={this.state.ClassNameStrel._5[0] ? "sec__body__spis_one___rec_us nejdan active__icon active_str_n" : "sec__body__spis_one___rec_us pasiv_us_strel"}
 							onClick={this.usstr}></div>
 						<div className={this.state.ClassNameStrel._5[2].create_pl ? "video__block__open" : "video__block__close"} style={{ height: this.state.ClassNameStrel._5[1] }}>
-							{this.grid__video__system()}
+							{this.grid__video__system(this.state.ClassNameStrel._5)}
 						</div>
 					</div>
 
-					<div className="sec__body__spis_one___podpis">
+					<div className="sec__body__spis_one___podpis" style={{ top: this.funSwapMath(this.state.ClassNameStrel._6) }}>
 						<div className="sec__body__spis_one___rec_icon_6"></div>
 						<a href="#">Видео друзей</a>
 						<div id="_6" className={this.state.ClassNameStrel._6[0] ? "sec__body__spis_one___rec_us_l  active_icon_two" : "sec__body__spis_one___rec_us_l"}
 							onClick={this.usstr}></div>
 						<div className={this.state.ClassNameStrel._6[2].video_frend ? "video__block__open" : "video__block__close"} style={{ height: this.state.ClassNameStrel._6[1] }}>
-							{this.grid__video__system()}
+							{this.grid__video__system(this.state.ClassNameStrel._6)}
 						</div>
 					</div>
 
-					<div className="sec__body__spis_one___sech">
+					<div className="sec__body__spis_one___sech" style={{ top: this.funSwapMath(this.state.ClassNameStrel._7) }}>
 						<div className="sec__body__spis_one___rec_icon_7"></div>
 						<a href="#">Посмотреть позже</a>
 						<div id="_7" className={this.state.ClassNameStrel._7[0] ? "sec__body__spis_one___rec_us nejdan active__icon active_str_n" : "sec__body__spis_one___rec_us pasiv_us_strel"}
 							onClick={this.usstr}></div>
 						<div className={this.state.ClassNameStrel._7[2].pozje ? "video__block__open" : "video__block__close"} style={{ height: this.state.ClassNameStrel._7[1] }}>
-							{this.grid__video__system()}
+							{this.grid__video__system(this.state.ClassNameStrel._7)}
 						</div>
 					</div>
 
-					<div className="sec__body__spis_one___podpis">
+					<div className="sec__body__spis_one___podpis" style={{ top: this.funSwapMath(this.state.ClassNameStrel._8) }}>
 						<div className="sec__body__spis_one___rec_icon_8"></div>
 						<a href="#">История</a>
 						<div id="_8" className={this.state.ClassNameStrel._8[0] ? "sec__body__spis_one___rec_us_l  active_icon_two" : "sec__body__spis_one___rec_us_l"}
 							onClick={this.usstr}></div>
 						<div className={this.state.ClassNameStrel._8[2].history ? "video__block__open" : "video__block__close"} style={{ height: this.state.ClassNameStrel._8[1] }}>
-							{this.grid__video__system()}
+							{this.grid__video__system(this.state.ClassNameStrel._8)}
 						</div>
 					</div>
 
-					<div className="sec__body__spis_one___sech">
+					<div className="sec__body__spis_one___sech" style={{ top: this.funSwapMath(this.state.ClassNameStrel._9) }}>
 						<div className="sec__body__spis_one___rec_icon_9"></div>
 						<a href="#">Избранные</a>
 						<div id="_9" className={this.state.ClassNameStrel._9[0] ? "sec__body__spis_one___rec_us nejdan active__icon active_str_n" : "sec__body__spis_one___rec_us pasiv_us_strel"}
 							onClick={this.usstr}></div>
 						<div className={this.state.ClassNameStrel._9[2].serch_kanal ? "video__block__open" : "video__block__close"} style={{ height: this.state.ClassNameStrel._9[1] }}>
-							{this.grid__video__system()}
+							{this.grid__video__system(this.state.ClassNameStrel._9)}
 						</div>
 					</div>
 
-					<div className="sec__body__spis_one___podpis">
+					<div className="sec__body__spis_one___podpis" style={{ top: this.funSwapMath(this.state.ClassNameStrel._10) }}>
 						<div className="sec__body__spis_one___rec_icon_10"></div>
 						<a href="#">Свои записи</a>
 						<div id="_10" className={this.state.ClassNameStrel._10[0] ? "sec__body__spis_one___rec_us_l  active_icon_two" : "sec__body__spis_one___rec_us_l"}
 							onClick={this.usstr}></div>
 						<div className={this.state.ClassNameStrel._10[2].my_post ? "video__block__open" : "video__block__close"} style={{ height: this.state.ClassNameStrel._10[1] }}>
-							{this.grid__video__system()}
+							{this.grid__video__system(this.state.ClassNameStrel._10)}
 						</div>
 					</div>
 				</div>
