cff63b9f075816b54af18aaf86c8311fa196aaf5
pavelpave <arkhip-sergeev@mail.ru>
2019-03-07 19:19:37 +0300

    fix heder

--- src/ui/heder/heder.js
+++ src/ui/heder/heder.js
@@ -4,37 +4,37 @@ import './styleHeder.css';
 class Heder extends React.Component{
 	hederV = () => {
  		return (
- 				<heder>
+ 				<section>
  					<div className="polosa__heder">
  						<a href="#"><div className='header__logo'></div></a>
  						<nav>
-							<ul class="nav__menu__ul">
+							<ul className="nav__menu__ul">
 								<li>
 									<a href="#">Главная</a>
-									<div class="li_capl"></div>
+									<div className="li_capl"></div>
 								</li>
 								<li>
 									<a href="#">Сообщения</a>
-									<div class="li_capl"></div>
+									<div className="li_capl"></div>
 								</li>
 								<li>
 									<a href="#">Трансляции</a>
-									<div class="li_capl"></div>
+									<div className="li_capl"></div>
 								</li>
 								<li>
 									<a href="#">Анонимка</a>
-									<div class="li_capl"></div>
+									<div className="li_capl"></div>
 								</li>
 							</ul>
 						</nav>
-						<div class="heder__block__akk">
-							<p class="heder__block__name"><a href="#">Анастасия</a></p>
-							<div class="heder__block__img">
-								<div class="block__img"></div>
+						<div className="heder__block__akk">
+							<p className="heder__block__name"><a href="#">Анастасия</a></p>
+							<div className="heder__block__img">
+								<div className="block__img"></div>
 							</div>
 						</div>
 					</div>
- 				</heder>
+				</section>
  			)
  	}
 render(){
