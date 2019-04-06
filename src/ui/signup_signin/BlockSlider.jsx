3b5efbaa14f1c7a752ed71e49eb4278d903059bc
pavelpave <arkhip-sergeev@mail.ru>
2019-04-06 16:24:32 +0400

    sas

--- src/ui/signup_signin/BlockSlider.jsx
+++ src/ui/signup_signin/BlockSlider.jsx
@@ -7,12 +7,14 @@ export default function BlockSlider(props){
     return(
         <div className="block_slider" >
         <div className="one_slide block_slid_hover" >
+            <p className='oneSlidText'>Последние новости</p>
             <div className="one_card_out Out_clas"><p>Общяйся и будь <br />
                                                     вкурсе всего <br/>
                                                     самого интересного
             </p></div>
         </div>
         <div className="two_slide block_slid_hover" >
+             <div className="stichAnimation"></div>
              <div className="two_card_out Out_clas">
                 <h3 className='two_block_h3'>Создавай интересные стикеры <br/>
                                              и стань лучшим <br />
@@ -35,9 +37,10 @@ export default function BlockSlider(props){
             </div>
         </div>
         <div className="foor_slide block_slid_hover" >
-            <div className="block_kub"> 
-                 <div>hi pipl</div>  
-                    
+            <div className="block_foor_wrap"> 
+                 <input type="text" name="" id="" placeholder='Поиск' className='input_block_foor'/> 
+                 <button className="butCreateCanal">Создать анонимный канал</button>
+                 <button className="butCreateChat">Создать анонимный чат </button>
             </div>
         </div>
         <div className="six_slide block_slid_hover" >
